1a660efad090c22f7e06360c85dfa13a3a35e04e
Pawan Rai <rahunn3@gmail.com>
2020-12-30 14:40:48 +0545

    port setup for heroku

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //Import Packages
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const compression = require('compression');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
 
 //Initialize express server
 const app = express();
@@ -21,26 +21,26 @@ const productRoutes = require("./routes/Product");
 app.use(cors());
 app.use(helmet());
 app.use(compression());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //Implement Routes
-app.use('/post',postRoutes);
+app.use("/post", postRoutes);
 
-app.use('/todo',todoRoutes);
+app.use("/todo", todoRoutes);
 
-app.use('/user', userRoutes);
+app.use("/user", userRoutes);
 
-app.use('/product', productRoutes);
+app.use("/product", productRoutes);
 
 //Catch error
 app.use((error, req, res, next) => {
-    console.log(error);
-    const status = error.statusCode;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({ message: message, data: data });
-  });
+  console.log(error);
+  const status = error.statusCode;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
 
 //Run server at port 3000
-app.listen('3000');
+app.listen(process.env.PORT || 3000);
 module.exports = app;
